71ddead4186638bc7ba4544b0c5eb8bf6870c431
Charles West <crwest@ncsu.edu>
2018-04-10 11:15:08 -0400

    Got it compiling and linking to libbitcoin.

--- src/executables/testProgram/main.cpp
+++ src/executables/testProgram/main.cpp
@@ -1,13 +1,10 @@
 #include<iostream>
 #include <bitcoin/bitcoin.hpp>
 
-using namespace bc::chain;
-using namespace bc::config;
-
 int main(int argc, char** argv)
 {
 
-endorsement endorse;
+libbitcoin::endorsement endorse;
 
 
 return 0;
---
+++ src/library/bufferFunction.cpp
@@ -0,0 +1,4 @@
+int returnOne()
+{
+    return 1;
+}
