554ed2c5e7e486de6e115e436e87b72f85d67376
Charles West <crwest@ncsu.edu>
2018-04-10 10:56:59 -0400

    Initial commit.

---
+++ src/executables/test/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <bitcoin/bitcoin.hpp>
+
+using namespace bc::chain;
+using namespace bc::config;
+
+int main(int argc, char** argv)
+{
+
+endorsement endorse;
+
+
+return 0;
+}
